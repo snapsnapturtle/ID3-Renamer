c1273849e0021bd146739e535b07f5f792ab4047
Jonah Möller <jonah.moeller@outlook.com>
2014-11-08 16:25:03 +0100

    added listing functionality

--- ID3Renamer/Form1.Designer.cs
+++ ID3Renamer/Form1.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listDirectories = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBoxParentDirectory = new System.Windows.Forms.TextBox();
+            this.buttonBrowseParentDirectory = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(364, 200);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listDirectories
+            // 
+            this.listDirectories.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listDirectories.FormattingEnabled = true;
+            this.listDirectories.Location = new System.Drawing.Point(12, 38);
+            this.listDirectories.Name = "listDirectories";
+            this.listDirectories.Size = new System.Drawing.Size(427, 145);
+            this.listDirectories.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Firebrick;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 233);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(451, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.ForeColor = System.Drawing.Color.White;
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(64, 17);
+            this.labelStatus.Text = "labelStatus";
+            // 
+            // textBoxParentDirectory
+            // 
+            this.textBoxParentDirectory.Location = new System.Drawing.Point(12, 12);
+            this.textBoxParentDirectory.Name = "textBoxParentDirectory";
+            this.textBoxParentDirectory.Size = new System.Drawing.Size(397, 20);
+            this.textBoxParentDirectory.TabIndex = 3;
+            // 
+            // buttonBrowseParentDirectory
+            // 
+            this.buttonBrowseParentDirectory.Location = new System.Drawing.Point(415, 11);
+            this.buttonBrowseParentDirectory.Name = "buttonBrowseParentDirectory";
+            this.buttonBrowseParentDirectory.Size = new System.Drawing.Size(24, 22);
+            this.buttonBrowseParentDirectory.TabIndex = 4;
+            this.buttonBrowseParentDirectory.Text = "...";
+            this.buttonBrowseParentDirectory.UseVisualStyleBackColor = true;
+            this.buttonBrowseParentDirectory.Click += new System.EventHandler(this.buttonBrowseParentDirectory_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 316);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(451, 255);
+            this.Controls.Add(this.textBoxParentDirectory);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listDirectories);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonBrowseParentDirectory);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listDirectories;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.TextBox textBoxParentDirectory;
+        private System.Windows.Forms.Button buttonBrowseParentDirectory;
     }
 }
 
--- ID3Renamer/Form1.cs
+++ ID3Renamer/Form1.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ID3Renamer
 {
     public partial class Form1 : Form
     {
+
+        int fileCount;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            labelStatus.Text = "Getting MP3 Files";
+
+            string directory = textBoxParentDirectory.Text;
+            processDirectory(directory);
+        }
+
+        public void processDirectory(string directory)
+        {
+            listDirectories.Items.Clear();
+
+            if (Directory.Exists(directory))
+            {
+                string[] files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    listDirectories.Items.Add(file);
+                    fileCount++;
+                }
+
+                if (fileCount == 1) labelStatus.Text = fileCount + " Item Loaded";
+                else labelStatus.Text = fileCount + " Items Loaded";
+            }
+
+            else
+            {
+                labelStatus.Text = "Directory is not valid";
+            }
+        }
+
+        private void buttonBrowseParentDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browseDirectory = new FolderBrowserDialog();
+            if (browseDirectory.ShowDialog() == DialogResult.OK)
+            {
+                string directory = browseDirectory.SelectedPath;
+                textBoxParentDirectory.Text = directory;
+                processDirectory(directory);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxParentDirectory.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         }
     }
 }
